3b6ebb9f47b561d6959c45b5c54bc728568f9afb
jefflueck <lueckcfd@yahoo.com>
2025-02-17 19:25:37 -0600

    added new script, edited Player.cs script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -20,6 +20,7 @@ public class Player : MonoBehaviour
     private float _canFire = -1f;
     [SerializeField]
     private int _lives = 3;
+    private SpawnManager _spawnManager;
 
 
     // Start is called before the first frame update
@@ -27,6 +28,11 @@ public class Player : MonoBehaviour
     {
         // Take current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+        if (_spawnManager == null)
+        {
+            Debug.LogError("Spawn Manager is NULL");
+        }
     }
 
     // Update is called once per frame
@@ -85,6 +91,7 @@ public class Player : MonoBehaviour
 
         if (_lives < 1)
         {
+            _spawnManager.OnPlayerDeath();
             Destroy(this.gameObject);
         }
 
